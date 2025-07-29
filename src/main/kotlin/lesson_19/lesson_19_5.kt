f54d2c8a216a75fea014765795ab4ca86d588f77
lion88 <lion_alex1988@mail.ru>
2025-07-29 23:39:59 +0300

    KS-19-5 second option

--- src/main/kotlin/lesson_19/lesson_19_5.kt
+++ src/main/kotlin/lesson_19/lesson_19_5.kt
@@ -1,10 +1,9 @@
 package lesson_19
 
-import java.util.*
-
 enum class Gender {
     MALE,
-    FEMALE
+    FEMALE,
+    OTHER
 }
 
 class Person(val name: String, val gender: Gender) {
@@ -15,32 +14,35 @@ class Person(val name: String, val gender: Gender) {
 
 fun main() {
     val personList = mutableListOf<Person>()
-    val scanner = Scanner(System.`in`)
     println("Добро пожаловать в Картотеку!")
     println("Введите имя и пол человека в формате: Имя Пол")
     println("Доступные значения для пола: ${Gender.entries.joinToString(", ") { it.name }}")
+    repeat(5) { i ->
+        while (true) {
+            print("Введите данные человека (${i + 1}/5): ")
+            val input = readln().split(" ")
+            if (input.size != 2) {
+                println("Ошибка: Неверный формат ввода. Пожалуйста, введите имя и пол через пробел.")
+                continue
+            }
+            val name = input[0]
+            val genderString = input[1].uppercase()
+            val gender: Gender? = try {
+                Gender.valueOf(genderString)
+            } catch (e: IllegalArgumentException) {
+                null
+            }
 
-    while (personList.size < 5) {
-        print("Введите данные человека (${personList.size + 1}/5): ")
-        val input = scanner.nextLine().split(" ")
-        if (input.size != 2) {
-            println("Ошибка: Неверный формат ввода. Пожалуйста, введите имя и пол через пробел.")
-            continue
+            if (gender == null) {
+                println("Ошибка: Неверное значение для пола. Используйте: ${Gender.entries.joinToString(", ") { it.name }}")
+                continue
+            }
+
+            val person = Person(name, gender)
+            personList.add(person)
+            println("Человек добавлен в картотеку.")
+            break
         }
-        val name = input[0]
-        val genderString = input[1].uppercase()
-        val gender: Gender? = try {
-            Gender.valueOf(genderString)
-        } catch (e: IllegalArgumentException) {
-            null
-        }
-        if (gender == null) {
-            println("Ошибка: Неверное значение для пола. Используйте: ${Gender.entries.joinToString(", ") { it.name }}")
-            continue
-        }
-        val person = Person(name, gender)
-        personList.add(person)
-        println("Человек добавлен в картотеку.")
     }
     println("\nКартотека:")
     personList.forEach { person -> person.displayInfo() }
