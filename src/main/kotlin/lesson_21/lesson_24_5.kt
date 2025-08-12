2300dbbaac0b883bed5175cdb4d598a281668e2c
lion88 <lion_alex1988@mail.ru>
2025-08-12 14:08:00 +0300

    KS-21-5 second option

--- src/main/kotlin/lesson_21/lesson_24_5.kt
+++ src/main/kotlin/lesson_21/lesson_24_5.kt
@@ -1,24 +1,11 @@
 package lesson_21
 
 fun Map<String, Int>.maxCategory(): String? {
-    if (isEmpty()) {
-        return null
-    }
-
-    var maxCategory: String? = null
-    var maxExperience = Int.MIN_VALUE
-
-    for ((category, experience) in this) {
-        if (experience > maxExperience) {
-            maxExperience = experience
-            maxCategory = category
-        }
-    }
-    return maxCategory
+        return maxByOrNull { it.value }?.key
 }
 
 fun main() {
-    val skillMap1 = mapOf("Сила" to 150, "Ловкость" to 120, "Интеллект" to 100)
+    val skillMap1 = mapOf("Сила" to 150, "Ловкость" to 180, "Интеллект" to 100)
     val skillMap2 = mapOf("Сила" to 100, "Ловкость" to 100, "Интеллект" to 100)
     val skillMap3 = emptyMap<String, Int>()
 
