f527cc5e19caca7c2f96af0aaad6b03609f6d787
lion88 <lion_alex1988@mail.ru>
2025-08-13 20:15:41 +0300

    KS-22-2 second option

--- src/main/kotlin/lesson_22/lesson_22_2.kt
+++ src/main/kotlin/lesson_22/lesson_22_2.kt
@@ -8,5 +8,9 @@ fun main() {
     val regularBook2 = RegularBook2("The Fellowship of the Ring", "Tolkien")
     val dataBook2 = DataBook2("The Two Towers", "Tolkien")
     println("RegularBook2: $regularBook2")
+    // Обычные классы по умолчанию не имеют переопределённого метода toString().
+    // Dыводится представление объекта, а не его содержимое.
     println("DataBook2: $dataBook2")
+    // Data классы автоматически генерируют метод toString(), который выводит значения всех свойств класса.
+    // Это позволяет увидеть, какие данные хранятся в объекте.
 }
